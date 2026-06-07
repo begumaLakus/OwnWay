53d514606c5659d49917b20e4540de1dcd86818f
MelikeArslan <melike.arslan848@gmail.com>
2026-06-07 17:25:47 +0300

    Update app icons

--- mobile-app/src/screens/api.js
+++ mobile-app/src/screens/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://10.108.205.168:3000/api';
+const API_URL = 'http://10.108.181.232:3000/api';
 
 export const api = axios.create({
   baseURL: API_URL,
