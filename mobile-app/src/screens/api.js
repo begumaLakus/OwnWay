3ac9ad08ccb8a2911657b9c3b2a4afec6c26dea4
begumaLakus <alakusbegum5@gmail.com>
2026-06-10 19:33:43 +0300

    API üzerinde ufak düzeltme

--- mobile-app/src/screens/api.js
+++ mobile-app/src/screens/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+<<<<<<< Updated upstream
 const API_URL = 'http://10.37.84.55:3000/api';
+=======
+const API_URL = 'http://192.168.1.100:3000/api';
+>>>>>>> Stashed changes
 
 export const api = axios.create({
   baseURL: API_URL,
