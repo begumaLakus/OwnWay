41e00f4f42064b39e334e796a4a6b449ec1a431a
nisa cibik <nisacibikk@gmail.com>
2026-06-11 16:25:54 +0300

    conflict çözüldü

--- mobile-app/src/screens/api.js
+++ mobile-app/src/screens/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+<<<<<<< Updated upstream
 const API_URL = 'http://192.168.1.244:3000/api';
+=======
+const API_URL = 'http://192.168.1.110:3000/api';
+>>>>>>> Stashed changes
 
 export const api = axios.create({
   baseURL: API_URL,
